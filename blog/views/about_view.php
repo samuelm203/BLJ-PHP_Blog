ba5fd0dc0c78acf9059bf36bccdc04ca003a2be4
Samuel Ming <samuelming09@gmail.com>
2025-11-27 11:41:56 +0100

    Initial Commit

--- blog/views/about_view.php
+++ blog/views/about_view.php
@@ -7,13 +7,32 @@
 
     <div class="about-card">
         <h2 class="about-card-titel">Samuel Ming</h2>
-        <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-            ipsum dolor sit amet. Lorem ipsum dolor
-            <br>
-            sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-            magna aliquyam erat, sed diam voluptua.
-            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <p> I’m an apprentice IT application developer who loves building things that actually work and make life easier.
+            I enjoy exploring new technologies, learning by doing, and improving my skills one project at a time.
+        </p>
+        <h3 class="about-card-mysocial-title">My Socials</h3>
+        <div class="about-card-mysocial">
+            <a class="about-card-mysocial-link" href="https://github.com/samuelm203" target="_blank">
+            <img width="20px"
+                 src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                 alt="GitHub Logo">
+            </a>
+            <a class="about-card-mysocial-link" href="https://www.linkedin.com/in/samuel-ming-7b528a261/" target="_blank">
+                <img width="20px"
+                     src="https://www.svgrepo.com/show/110227/linkedin-big-logo.svg"
+                     alt="LinkedIn Logo">
+            </a>
+            <a class="about-card-mysocial-link" href="https://discord.com" target="_blank">
+                <img width="20px"
+                     src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/discord-round-black-icon.png"
+                     alt="Discord Logo">
+            </a>
+            <a class="about-card-mysocial-link" href="https://stackoverflow.com/questions" target="_blank">
+                <img width="20px"
+                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2aAgizvWEPoY6iZWUKSd_GE1eENXRKgHpjA&s"
+                     alt="Stack Overflow Logo">
+            </a>
+        </div>
     </div>
 </div>
 
